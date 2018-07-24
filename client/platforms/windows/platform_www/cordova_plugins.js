2a45317e185393860474bcb50929a709bd640095
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2018-07-24 17:51:06 +0300

    Add server side and angular application

--- client/platforms/windows/platform_www/cordova_plugins.js
+++ client/platforms/windows/platform_www/cordova_plugins.js
@@ -1,8 +1,24 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.DeviceProxy",
+        "file": "plugins/cordova-plugin-device/src/windows/DeviceProxy.js",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-device": "2.0.2",
     "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
